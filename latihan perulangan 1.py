25108379cab2ad7c0abf81c435ff5b7346873413
Syamsul <syamsularifin18@gmail.com>
2021-11-25 16:22:49 +0900

    perulangan 1

---
+++ latihan perulangan 1.py
@@ -0,0 +1,10 @@
+
+jumlah_buku = 10
+print("ibu menyuruh membaca semua buku")
+
+jumlah_buku_yang_ude_dibaca = 0
+print("jumlah_buku_yang_ude_dibaca")
+
+for jumlah_buku_yang_ude_dibaca in range(1,jumlah_buku+1):
+    print(f"buku yang ke-{jumlah_buku_yang_ude_dibaca} ude dibaca")
+print(jumlah_buku_yang_ude_dibaca)
