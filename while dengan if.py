3780c6cd3cd10dd3364de8214141a924afec392e
Syamsul <syamsularifin18@gmail.com>
2021-11-25 19:38:18 +0900

    while if

--- while dengan if.py
+++ while dengan if.py
@@ -1,12 +1,7 @@
 
 jumlah_buku = 10
-print("ibu menyuruh membaca dan memahami semua buku")
-
 total_jumlah_baca_buku = 0
-print(f"total jumlah baca buku{total_jumlah_baca_buku}")
-
 buku_yang_udah_dibaca_dan_dipahami = 0
-print(f'buku yang udah dibaca dan dipahami {buku_yang_udah_dibaca_dan_dipahami}')
 
 while total_jumlah_baca_buku < jumlah_buku * 2 :
     total_jumlah_baca_buku = total_jumlah_baca_buku + 1
