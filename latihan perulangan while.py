ad71d06f36b18774bd8b2d70eaff333eb2420f86
Syamsul <syamsularifin18@gmail.com>
2021-11-25 18:17:37 +0900

    while

---
+++ latihan perulangan while.py
@@ -0,0 +1,14 @@
+
+jumlah_buku = 10
+print("ibu menyuruh membaca semua buku")
+
+buku_yang_udah_dibaca = 0
+print("buku yang udah dibaca")
+
+while buku_yang_udah_dibaca < jumlah_buku:
+    buku_yang_udah_dibaca = buku_yang_udah_dibaca + 1
+    print(f"buku yang ke-{buku_yang_udah_dibaca}udeh dibaca")
+
+print(f"jumlah buku yang udah di baca {buku_yang_udah_dibaca}")
+
+
