4910b5dcd1c47535eb1321bd7404b3efd96b840d
Syamsul <syamsularifin18@gmail.com>
2021-11-26 05:26:11 +0900

    data list

--- latihan data list.py
+++ latihan data list.py
@@ -29,3 +29,22 @@ print(daftar_buku[4])
 print("\ntampilkan mengunakan len")
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print("cara mengganti list")
+daftar_buku = ["alqura'n", "iqro", "sansu", "seikatsu","kanji"]
+daftar_buku[2] = "komik"
+print(daftar_buku)
+
+print("\ncara menghapus list")
+daftar_buku.pop(3)
+print(daftar_buku)
+
+print("\ncara menghapus list dari belakang")
+daftar_buku.pop(-2)
+print(daftar_buku)
+
+daftar_buku = ["alqura'n", "iqro", "sansu", "seikatsu","kanji"]
+print("\n cra menghapus list dari belakang hanya 1 buku")
+daftar_buku.pop(-2)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
